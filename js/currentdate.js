72b0b57b6188000e0697b130e5714c716c4611b2
fleshyy <shanewrede@gmail.com>
2021-04-24 22:51:10 +0200

    commit all?

---
+++ js/currentdate.js
@@ -0,0 +1,9 @@
+
+const d = new Date();
+
+const currentYear = d.getFullYear();
+
+const modified = document.lastModified
+
+document.getElementById('lastModified').textContent = modified;
+document.querySelector('#currentYear').textContent = currentYear;
